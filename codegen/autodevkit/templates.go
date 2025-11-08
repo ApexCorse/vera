3025281568c598b9f5a8df6a77b5ca2b738903d7
Antonio Lentini <111151429+lentscode@users.noreply.github.com>
2025-11-08 18:04:11 +0100

    fix: fix typos

--- codegen/autodevkit/templates.go
+++ codegen/autodevkit/templates.go
@@ -7,18 +7,18 @@ const (
 #include "vera.h"
 #include "can_lld.h"
 
-vera_err_t vera_parse_autodevkit_rx_frame(CANRxFrame* frame, vera_decoding_result_t* result);
+vera_err_t vera_decode_autodevkit_rx_frame(CANRxFrame* frame, vera_decoding_result_t* result);
 
 #endif // VERA_AUTODEVKIT_H`
 	sourceFile = `#include "vera_autodevkit.h"
 #include <string.h>
 
-vera_err_t vera_parse_autodevkit_rx_frame(CANRxFrame* frame, vera_decoding_result_t* result) {
+vera_err_t vera_decode_autodevkit_rx_frame(CANRxFrame* frame, vera_decoding_result_t* result) {
 	vera_can_rx_frame_t vera_frame = {
 		.id             = frame->ID,
 		.dlc            = frame->DLC * 8,
 		.is_extended_id = frame->TYPE,
-		.is_fd          = frame->OPERATION == 0x01U ? true : false,
+		.is_fd          = frame->OPERATION == 0x01U ? true : false
 	};
 	memcpy(vera_frame.data, frame->data8, frame->DLC);
 
